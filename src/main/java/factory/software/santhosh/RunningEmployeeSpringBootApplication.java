dbef1ac24fd990a6d6aa092f0f8dbcb58f77f932
manoj <santhosh15071997@gmail.com>
2020-08-28 16:08:19 +0530

    commit new next

--- src/main/java/factory/software/santhosh/RunningEmployeeSpringBootApplication.java
+++ src/main/java/factory/software/santhosh/RunningEmployeeSpringBootApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RunningEmployeeSpringBootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello spring boot");
+		int a=0;
+		System.out.println("hello spring boot"+a);
 		SpringApplication.run(RunningEmployeeSpringBootApplication.class, args);
 	}
 
